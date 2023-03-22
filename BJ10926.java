18c94b2731b45f283e06efa9b926667fc848e844
JeongSWsw <jeongsw34@gmail.com>
2023-03-23 00:06:57 +0900

    BJ10926

---
+++ BJ10926.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = bufferedReader.readLine();
+
+        System.out.println(str+"??!");
+    }
+}
