9dd3ce3a9f9f4bdcd6b7caaa8d979805e3928faa
JeongSWsw <jeongsw34@gmail.com>
2023-03-22 23:39:51 +0900

    BJ10869

---
+++ BJ10869.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = bufferedReader.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(str," ");
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
